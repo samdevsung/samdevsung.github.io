ea6183415c29a33dff478c90537d0c45f783bce8
Devesh Singh <samdevsung@gmail.com>
2023-07-17 22:22:00 +0530

    first commit

---
+++ firstWebsite/app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+const inola = document.querySelector(".main__content h1")
+inola.textContent= "NEXT GENERATION"
